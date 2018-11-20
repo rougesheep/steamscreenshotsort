3b106db5a5a4a516b3000e5ccb7360b26f9fe18c
Andy Burne <rougesheep@gmail.com>
2018-11-20 22:54:23 +0000

    works!

--- steam_screenshot_sort.ps1
+++ steam_screenshot_sort.ps1
@@ -1,6 +1,6 @@
 $config = @{
-    screenshot_folder = "$env:USERPROFILE\Pictures\Test"
-    output_folder = "$env:USERPROFILE\Pictures\Test\Out"
+    screenshot_folder = "$env:USERPROFILE\Pictures\Steam"
+    output_folder = "$env:USERPROFILE\Pictures\Steam"
     steam_api = "https://api.steampowered.com/ISteamApps/GetAppList/v0002/"
     cache_file = "steam_games.json"
 }
@@ -13,17 +13,17 @@ if (-not (Test-Path $config['output_folder'])){
     New-Item -ItemType Directory -Path $config['output_folder']
 }
 
-$catalog = Get-Content -Raw -Path $config['cache_file'] | ConvertFrom-Json
+$catalog = Get-Content -Encoding UTF8 -Raw -Path $config['cache_file'] | ConvertFrom-Json
 
 $files = Get-ChildItem $config['screenshot_folder'] -Filter *.png | Select-Object -ExpandProperty Name
 
 ForEach($file in $files ){
     $appID = $file.Split('_')[0]
     $appName = $catalog.applist.apps | Where-Object {$_.appid -eq $appID} | Select-Object -ExpandProperty name
-    # $copyIn = Join-Path $config['screenshot_folder'] $file
-    # $copyOut = Join-Path $config['output_folder'] $appName
-    # if (-not (Test-Path $copyOut)){
-    #     New-Item -ItemType Directory -Path $copyOut
-    # }
-    #Copy-Item -Path "$($config.screenshot_folder)\$file" -Destination "$($config.output_folder)\$appName"
+    $appName = $appName -replace '\<|\>|:|"|/|\\|\||\?|\*', ''
+    $copyOut = Join-Path $config['output_folder'] $appName
+    if (-not (Test-Path $copyOut)){
+        New-Item -ItemType Directory -Path $copyOut
+    }
+    Move-Item -Path "$($config.screenshot_folder)\$file" -Destination "$($config.output_folder)\$appName"
 }
